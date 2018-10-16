d8089aaa1c7509860c220236edfb3a275e4c5220
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-10-16 11:48:26 -0300

    Criação do componente de despesas

---
+++ src/Expenses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Expenses extends Component {
+    render() {
+        return(
+            <h1>Olá!</h1>
+        );
+    }
+}
+
+export default Expenses;
