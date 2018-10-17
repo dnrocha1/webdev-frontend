88bcd973da290924738051f68da435aa46591a51
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-10-17 17:41:42 -0300

    Utilizando Material UI p/ estilizar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Feed from "./pages/Feed";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
     render() {
         return (
-            <Feed />
+            <div>
+                <Navbar>Easy Expend</Navbar>
+                <Feed/>
+            </div>
         );
     }
 }
--- src/components/FriendBalance.js
+++ src/components/FriendBalance.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from '@material-ui/core/Typography';
+
 /**
  * This component displays the current user's balance with a friend.
  */
@@ -7,18 +11,22 @@ class FriendBalance extends Component {
     constructor() {
         super();
     }
-
+    
     render() {
         const expense = this.props.expense;
         console.log(expense)
         return (
             <div align="center">
-                <div className="expense">
-                    <span className="user">{expense.user}</span>
-                    <br/>
-                    <span className="balance">{expense.balance}</span>
-                </div>
-                <br/>
+            <Card>
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                        {expense.user}
+                    </Typography>
+                    <Typography color="textSecondary">
+                        {expense.balance}
+                    </Typography>
+                </CardContent>
+            </Card>
             </div>
         );
     }
