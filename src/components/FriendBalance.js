55c8c85d33af696bdefc2b0215875e04e92fb153
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2018-11-21 16:28:29 -0300

    Remoção do estilo anterior

--- src/components/FriendBalance.js
+++ src/components/FriendBalance.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from '@material-ui/core/Typography';
-
 /**
  * This component displays the current user's balance with a friend.
  */
@@ -17,16 +13,9 @@ class FriendBalance extends Component {
         console.log(expense)
         return (
             <div align="center">
-            <Card>
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        {expense.user}
-                    </Typography>
-                    <Typography color="textSecondary">
-                        {expense.balance}
-                    </Typography>
-                </CardContent>
-            </Card>
+                {expense.user} : {expense.balance}
+                <br/>
+                <br/>
             </div>
         );
     }
