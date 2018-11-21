ac20b9004bec94755d924525b8bd555c9c00bbb6
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2018-11-21 17:36:08 -0300

    Estilização da navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
-import { Menu } from 'semantic-ui-react';
+import { Menu, Dropdown } from 'semantic-ui-react';
 
 export default class Navbar extends Component {
-    state = {};
+    state = { activeItem: 'home'};
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
     render() {
         const { activeItem } = this.state;
 
         return (
-            <Menu>
-                <Menu.Item name='editorials' active={activeItem === 'editorials'} onClick={this.handleItemClick}>
-                Editorials
+            <Menu color='teal' inverted size='huge'>
+                <Menu.Item name='title' active={activeItem === 'title'} onClick={this.handleItemClick}>
+                { this.props.children }
                 </Menu.Item>
-
-                <Menu.Item name='reviews' active={activeItem === 'reviews'} onClick={this.handleItemClick}>
-                Reviews
-                </Menu.Item>
-
-                <Menu.Item name='upcomingEvents' active={activeItem === 'upcomingEvents'} onClick={this.handleItemClick}>
-                Upcoming Events
-                </Menu.Item>
+                
+                <Menu.Menu position='right'>
+                    <Dropdown item text='Adicionais'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Perfil</Dropdown.Item>
+                            <Dropdown.Item>Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Menu.Item></Menu.Item>
+                </Menu.Menu>
             </Menu>
         );
     }
 }
-
-//export default Navbar;
